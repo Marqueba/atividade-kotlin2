f1a818d3eaacf66ee1f39f30aac8d5486bb9d2a0
Marcos <reis17265@gmail.com>
2023-04-12 19:06:04 -0400

    Atividade finalizada

--- Array.kt
+++ Array.kt
@@ -1,13 +1,47 @@
 fun main () {
-    /* Exercícios Array
+    /* Exercício Array
     *   1 - Escreva um programa que crie um array de 10 números inteiros aleatórios e imprima o maior número do array.
     *   2 - Escreva um programa que crie um array de 10 números inteiros digitados pelo usuário e imprima a média dos números do array.
     *   3 - Escreva um programa que crie um array de 10 strings com nomes de frutas e imprima somente as frutas que começam com a letra "a" usando loop for e if.
     * */
 
-    // Exercícios 1
-    var lista = Array(10) {  }
-    // Exercícios 2
+    // Exercício 1
+    var lista = Array(10) { (0..100).random() }
+    var maior:Int = lista[0]
+    for (x in lista) {
+        if (x > maior) {
+            maior = x
+        }
+    }
+    println(lista.contentToString())
+    println("O maior é $maior")
+    println(maior)
+    // Exercício 2
+    var arrayInteiro = intArrayOf()
+    for (x in 0 .. 9) {
+        print("Digite o ${x+1}º Número: ")
+        var num = readln().toInt()
+        arrayInteiro = arrayInteiro.plus(num)
+    }
+    val soma = arrayInteiro.sum()
+    val media = soma/10
 
-    // Exercícios 3
+    print("A média dos números fonercidos é igual a $media")
+
+    // Exercício 3
+    var frutas = arrayOf("banana", "maçã", "melancia", "abacaxi", "abacate",
+    "jabuticaba", "laranja", "nectarina", "manga", "uva")
+
+    var listaFrutas = emptyArray<String>()
+
+    for (x in frutas) {
+        if (x[0] == 'a') {
+            listaFrutas += x
+        } else {
+            continue
+        }
+    }
+
+    println("Essas são as frutas que iniciam com \"a\": \n ${listaFrutas.contentToString()}")
+
 }
---
+++ out/production/atividade-kotlin2/README.md
@@ -0,0 +1,22 @@
+# :memo: Exercícios Kotlin - Semana 2
+
+### Temas abordados na aula
+<hr>
+
+##### ``While``;
+##### ``Do-While``;
+##### ``Loop-For``;
+##### ``Range``;
+##### ``Conitnue, Break & Repeat``;
+##### ``Array``;
+
+
+## 🚀 Tecnologias
+
+Essa atividade foi desenvolvido com as seguintes tecnologias:
+
+- ``Kotlin``
+
+## Contato
+
+| [<img src="https://avatars.githubusercontent.com/u/112917982?v=4" width=115><br><sub>Marcos Reis Dutra</sub>](https://github.com/Marqueba)
